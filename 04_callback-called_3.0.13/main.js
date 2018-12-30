b1dee0a4b87855f46ffed09ef868cfd6971ae484
Christian Dreier <flipper.x3r@googlemail.com>
2018-12-31 00:16:56 +0100

    Init

---
+++ 04_callback-called_3.0.13/main.js
@@ -0,0 +1,22 @@
+const electron = require("electron")
+
+electron.app.on("ready", () => {
+    mainWindow = new electron.BrowserWindow()
+    mainWindow.loadFile("index.html")
+
+    electron.Menu.setApplicationMenu(electron.Menu.buildFromTemplate([
+        {
+            label: "File",
+            submenu: [
+                {
+                    label: "Print",
+                    accelerator: "Ctrl+P",
+                    click() {
+                        mainWindow.webContents.print({}, success =>
+                            console.log(`Printed (${success ? "succeeded" : "failed"})`))
+                    }
+                }
+            ]
+        }
+    ]))
+})
